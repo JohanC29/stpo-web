daac75f2ebd6e27ec032f9571d96cdd0ca42172c
Johan Camilo Muelas <jc.muelasvernaza@gmail.com>
2021-09-14 00:04:14 -0500

    Se cambia BD

--- lib/conf/conf.php
+++ lib/conf/conf.php
@@ -1,9 +1,13 @@
 <?php
 
-    $server="remotemysql.com";
-    $user="bcc89DNzhq";
-    $pass="8PdqUOvN4O";
-    $database="bcc89DNzhq";
+    // $server="remotemysql.com";
+    // $user="bcc89DNzhq";
+    // $pass="8PdqUOvN4O";
+    // $database="bcc89DNzhq";
+    // $port="3306";
+    $server="sql10.freesqldatabase.com";
+    $user="sql10435502";
+    $pass="2WJ3QjGdje";
+    $database="sql10435502";
     $port="3306";
-    
 ?>
