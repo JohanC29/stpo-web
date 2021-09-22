27721b4744686a63ba25656b019be612ba494049
Johan Camilo Muelas <jc.muelasvernaza@gmail.com>
2021-09-22 00:13:33 -0500

    Se añade conexion nueva BD. Se inicia Modulo Proceso.

--- lib/conf/conf.php
+++ lib/conf/conf.php
@@ -1,13 +1,15 @@
 <?php
-
-    // $server="remotemysql.com";
-    // $user="bcc89DNzhq";
-    // $pass="8PdqUOvN4O";
-    // $database="bcc89DNzhq";
-    // $port="3306";
-    $server="sql10.freesqldatabase.com";
-    $user="sql10435502";
-    $pass="2WJ3QjGdje";
-    $database="sql10435502";
+    /*
+    $server="156.67.73.0";
+    $user="u425197612_stpo";
+    $pass="123456JohanC";
+    $database="u425197612_stpo";
     $port="3306";
+    */
+    $server="sql10.freesqldatabase.com";
+    $user="sql10439094";
+    $pass="gBhtFi5Wns";
+    $database="sql10439094";
+    $port="3306";
+    
 ?>
